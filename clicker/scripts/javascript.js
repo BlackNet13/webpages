b8578222dec3b4305572602ff1464d8659bd0b7f
BlackNet13 <123053395+BlackNet13@users.noreply.github.com>
2023-06-12 19:45:09 +0800

    update3

--- clicker/scripts/javascript.js
+++ clicker/scripts/javascript.js
@@ -5,44 +5,58 @@ function update(){
 		document.title = count + " clicks";
 		document.getElementById("ttAutoClick").innerHTML = autoclick;
 		document.getElementById("priceAutoClick").innerHTML = ((autoclick+1)*20);
-	}
+		document.getElementById("ttMulti").innerHTML = multi;
+		document.getElementById("priceMulti").innerHTML =((multi+1)*30);
+		document.getElementById("countPerSec").innerHTML = "Rate is: "+ parseInt((autoclick)+(multi*2)) + " /sec";
+}
 
-	var count = 0;
-	var autoclick =0;
+var count = 0;
+var autoclick =0;
+var multi = 0;
 	
-	function timer(){
-		count =count +autoclick;
-		update();
-	}
+function timer(){
+	count =count +autoclick;
+	count = count + multi*2;
+	update();
+}
 	
-	setInterval(timer, 1000);
-	//localStorage.clear();
+setInterval(timer, 1000);
+//localStorage.clear();
 	
 	
 //working functions
 function add(){
-				count = count+1;
-				update();
+		count = (count+1);	
+	update();
+}
+			
+			
+function save(){
+	localStorage.setItem("count", count);
+	localStorage.setItem("autoclick",autoclick);
 			}
 			
-			function save(){
-				localStorage.setItem("count", count);
-				localStorage.setItem("autoclick",autoclick);
-			}
-			
-			function load(){
-				count=parseInt(localStorage.getItem("count"));
-				document.getElementById("showAmt").value = count;
-				autoclick = parseInt(localStorage.getItem("autoclick"));
-				document.getElementById("ttAutoClick").value=autoclick;
+function load(){
+	count=parseInt(localStorage.getItem("count"));
+	document.getElementById("showAmt").value = count;
+	autoclick = parseInt(localStorage.getItem("autoclick"));
+	document.getElementById("ttAutoClick").value=autoclick;
 				
-				update();
-			}
+	update();
+}
 			
-			function buyAutoClick(){
-				if(count >=(autoclick+1)*20){
-					count = count -(autoclick+1)*20;
-					autoclick = autoclick +1;
-					update();
-				}
-			}
+function buyAutoClick(){
+	if(count >=(autoclick+1)*20){
+		count = count -(autoclick+1)*20;
+		autoclick = autoclick +1;
+		update();
+	}
+}
+
+function buyMulti(){
+	if(count >=(multi+1)*30){
+		count = count -(multi+1)*30;
+		multi = multi +1;
+		update();
+	}
+}
