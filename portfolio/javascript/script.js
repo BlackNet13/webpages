cdd351aeb18a5df824a5a2e776ec50763e0b8055
BlackNet13 <123053395+BlackNet13@users.noreply.github.com>
2023-09-04 22:14:48 +0800

    update6

---
+++ portfolio/javascript/script.js
@@ -0,0 +1,77 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    
+    
+     var logoLink = document.getElementById("logoLink");
+    
+        logoLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            var href = "index.html";
+            showTransitionBars(href);
+        });
+
+
+
+    window.goTo = function (event) {
+        event.preventDefault(); // Prevent the default link behavior
+        var href = event.target.getAttribute('href'); // Get the href attribute value
+
+        // Hide the body content with an opacity transition
+        document.getElementById("hero").style.transition = 'opacity 0.5s ease-in-out';
+        document.getElementById("hero").style.opacity = 0;
+        document.getElementById("transitionHolder").style.display = 'block';
+
+        // Delay the navigation after the opacity transition completes
+        setTimeout(function () {
+            showTransitionBars(href); // Show the transition bars after opacity transition
+        }, 500);
+    };
+
+
+    // Show the body content with an opacity transition when the page loads
+    document.getElementById("hero").style.transition = 'opacity 0.5s ease-in-out';
+    document.getElementById("hero").style.opacity = 1;
+
+
+// Add click event listeners to all <a> elements
+    var links = document.querySelectorAll('a');
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function (event) {
+            // Prevent the default link behavior and then trigger the transition bars animation
+            event.preventDefault();
+            window.goTo(event); // Call the goTo function
+        });
+    }
+
+    function showTransitionBars(href) {
+        const transitionBars = document.querySelectorAll('.transition-bars');
+
+        // Loop through each transition bar and show them with a delay
+        transitionBars.forEach((bar, index) => {
+            setTimeout(() => {
+                bar.style.display = 'block'; // Set display to 'block'
+            }, index * 800); // Delay each bar by 800 milliseconds
+        });
+
+        // Calculate the total duration of all bars
+        const totalDuration = transitionBars.length * 800;
+
+        // Simulate a delay before navigating to the specified href
+        setTimeout(() => {
+            window.location.href = href;
+        }, totalDuration);
+    }
+
+
+});
+
+
+
+
+
